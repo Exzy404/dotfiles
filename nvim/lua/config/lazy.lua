c1b921cee061865b9a15db0dd00e44332143cd52
Exzy404 <johngamerztx@gmail.com>
2026-05-04 10:40:06 -0400

    Cambios...

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,9 +17,14 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "oxocarbon",
-    } },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "oxocarbon",
+        clipboard = "unnamedplus",
+      },
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
